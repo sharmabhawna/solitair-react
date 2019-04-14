d42c1713e193541b19253f45556920b2ea2e780e
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-04-14 23:16:29 +0530

    Updates

---
+++ precache-manifest.dc4df195a19e4e8a43032cb990ae307a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "937104f6baf7ab872256",
+    "url": "/repository-name/static/js/main.f83c51ee.chunk.js"
+  },
+  {
+    "revision": "cb0e8d301fc236d7b6cd",
+    "url": "/repository-name/static/js/2.f0b0db37.chunk.js"
+  },
+  {
+    "revision": "937104f6baf7ab872256",
+    "url": "/repository-name/static/css/main.de0e19b5.chunk.css"
+  },
+  {
+    "revision": "4122437c11b8549e00b5d858d8212e39",
+    "url": "/repository-name/index.html"
+  }
+];
